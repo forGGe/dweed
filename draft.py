ca9d9290d633a64b0db217ce1ebed26d29b267ca
Max Payne <forgge@gmail.com>
2017-04-15 01:31:32 +0300

    Fix issue with reconnects

--- draft.py
+++ draft.py
@@ -10,9 +10,15 @@ class DweepyThreadListener(object):
     def __init__(self, thing, callback):
 
         def listen(thing, callback):
-            print('Start listening on ' + thing)
-            for dweet in dweepy.listen_for_dweets_from(thing):
-                callback(dweet)
+            while 1:
+                try:
+                    print('Start listening on ' + thing)
+                    for dweet in dweepy.listen_for_dweets_from(thing, timeout=20):
+                        callback(dweet)
+                except:
+                    print('Restarting listen due to exception')
+                    time.sleep(2)
+                    pass
 
         self.thread = threading.Thread(target=listen, args = (thing,callback,))
         self.thread.start()
@@ -20,40 +26,57 @@ class DweepyThreadListener(object):
 class Sensor(object):
     def __init__(self, discovery_thing):
         self.ctrl_data_lock = threading.Lock()
-        self.ctrl_data = collections.deque(maxlen = 1000)
+        self.ctrl_data = collections.deque(maxlen = 32)
         self.discovery_thing = discovery_thing
-        self.uuid_rt_data = discovery_thing + '.' + str(uuid.uuid1()) + '.rt_data'
-        self.uuid_ctrl = discovery_thing + '.' + str(uuid.uuid1()) + '.ctrl'
+        self.uuid = str(uuid.uuid1())
+        self.uuid_rt_data = discovery_thing + '.' + self.uuid + '.rt_data'
+        self.uuid_ctrl = discovery_thing + '.' + self.uuid + '.ctrl'
         self.listeners = []
 
         print('Registered sensor data stream on: ' + self.uuid_rt_data)
         print('Registered sensor control on: ' + self.uuid_ctrl)
-        print('Advertising sensor presence on: ' + self.discovery_thing)
-
-        dweepy.dweet_for(self.discovery_thing,
-        {
-            'method'    : 'advertise',
-            'type'      : 'sensor',
-            'rt_data'   : self.uuid_rt_data,
-            'ctrl'      : self.uuid_ctrl
-        })
 
         self.listeners.append(DweepyThreadListener(self.discovery_thing, self._lookup_callback))
         self.listeners.append(DweepyThreadListener(self.uuid_ctrl, self._ctrl_callback))
 
+        self._send_discovery()
+
+    def _send_discovery(self):
+        print('Advertising sensor presence on: ' + self.discovery_thing)
+        dweepy.dweet_for(self.discovery_thing,
+        {
+            'method'    : 'advertise',
+            'type'      : 'sensor',
+            'self'      : self.uuid,
+            'rt_data'   : self.uuid_rt_data,
+            'ctrl'      : self.uuid_ctrl
+        })
+
     def _lookup_callback(self, data):
-        print(data)
+        try:
+            method = data['content']['method']
+            type = data['content']['type']
+
+            if method == 'lookup' and type == 'sensor':
+                self._send_discovery()
+            else:
+                print('Discovery data received and ignored ' + str(data))
+        except:
+            print('Invalid data on the channel! ' + str(data))
 
     def _ctrl_callback(self, data):
         with self.ctrl_data_lock:
-            print(data)
+            self.ctrl_data.append(data)
 
     def send_data(self, data):
         dweepy.dweet_for(self.uuid_rt_data, data)
 
     def get_ctrl_data(self):
-        with self.ctrl_data_lock():
-            return self.ctrl_data.popleft()
+        with self.ctrl_data_lock:
+            try:
+                return self.ctrl_data.popleft()
+            except:
+                return None
 
 class DweetExchange(object):
     def __init__(self, type, discovery_thing):
@@ -63,15 +86,22 @@ class DweetExchange(object):
     def send_data(self, data):
         self.delegate.send_data(data)
 
-    def get_ctrl(self, data):
-        self.delegate.get_ctrl(data)
+    def get_ctrl_data(self):
+        return self.delegate.get_ctrl_data()
 
 discovery = 'd1e7a182-9f8a-440d-b9f8-13737b1e4f37'
 exch = DweetExchange('sensor', discovery)
 var = 1
 
 while True:
-    print('sending')
+    data = {'hello' : var}
+    print('Sending ' + str(data))
     var = var + 1
-    exch.send_data({'hello' : var})
-    time.sleep(1)  # Delay for 1 minute (60 seconds)
+    exch.send_data(data)
+
+    ctrl = exch.get_ctrl_data()
+
+    if ctrl:
+        print('Received ' + str(ctrl))
+
+    time.sleep(1)
