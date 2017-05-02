a506a8dd2e24afcf7e50febfa319a65579109efc
Max Payne <forgge@gmail.com>
2017-05-03 00:58:50 +0300

    Embed bokeh server inside application

--- draft.py
+++ draft.py
@@ -8,10 +8,19 @@ import random
 
 from threading import Thread
 
+from tornado.ioloop import IOLoop
+
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import curdoc, figure
 from bokeh.driving import linear
 from bokeh.client import push_session
+from bokeh.application.handlers import FunctionHandler
+from bokeh.application import Application
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, Slider
+from bokeh.server.server import Server
+from bokeh.server.server import BokehTornado
+
 
 from tornado import gen
 
@@ -32,21 +41,57 @@ def do_sensor():
 
         time.sleep(1)
 
+class Plotter:
+    def __init__(self):
+        self.i = 0
+        self.data = []
+
+    def add_point(self, x, y):
+        self.data.append({ 'x' : x, 'y' : y})
+
+    def start(self):
+        def plotter(doc):
+            # Base data
+            x = [ item['x'] for item in self.data ]
+            y = [ item['y'] for item in self.data ]
+
+            ds = ColumnDataSource(data=dict(x=x, y=y))
+
+            def update_plot():
+                new = {}
+                if len(self.data):
+                    new = { 'x' : [ self.data[-1]['x'] ], 'y' : [ self.data[-1]['y'] ] }
+
+                last = {}
+                if len(ds.data['x']):
+                    last = { 'x' : [ ds.data['x'][-1] ], 'y' : [ ds.data['y'][-1] ] }
+
+                if new != last:
+                    print('> New data arrived')
+                    ds.stream(new)
+
+            p = figure(plot_width=1200, plot_height=400)
+            p.line('x', 'y', color="firebrick", line_width=2, source=ds)
+
+            doc.add_periodic_callback(update_plot, 1000)
+
+            doc.add_root(p)
+
+        print('Plotter!')
+        io_loop = IOLoop.current()
+        bokeh_app = Application(FunctionHandler(plotter))
+        server = Server({'/': bokeh_app}, io_loop=io_loop, host="*")
+
+        server.start()
+
+        print('Opening Bokeh application on http://localhost:5006/')
+
+        io_loop.add_callback(server.run_until_shutdown)
+        io_loop.start()
+
 
 def do_view():
-    p = figure(plot_width=400, plot_height=400)
-    r = p.line([], [], color="firebrick", line_width=2)
-    ds = r.data_source
-    # curdoc().add_root(p)
-
-    # open a session to keep our local document in sync with server
-    session = push_session(curdoc())
-
-    def update_plot():
-        # print('> Update! ')
-        # ds.data['x'].append(random.randint(0,100))
-        # ds.data['y'].append(random.randint(0,100))
-        ds.trigger('data', ds.data, ds.data)
+    p = Plotter()
 
     def do_discovery():
         discovery = 'd1e7a182-9f8a-440d-b9f8-13737b1e4f37'
@@ -62,16 +107,12 @@ def do_view():
                 # Get some data
                 for data in view.listen_for_sensor_data(sensors[0]):
                     print(data)
-                    ds.data['x'].append(data['ts'])
-                    ds.data['y'].append(data['moisture'])
-
-    curdoc().add_periodic_callback(update_plot, 1000)
+                    p.add_point(data['ts'], data['moisture'])
 
     thread = Thread(target=do_discovery)
     thread.start()
 
-    session.show(p) # open the document in a browser
-    session.loop_until_closed() # run forever
+    p.start()
 
 if sys.argv[1] == 'view':
     do_view()
